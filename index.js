e73d3f025d5388a41ef0a54dd6489cc328bd9efc
Tyler Long <tyler4long@gmail.com>
2017-06-13 17:23:08 +0800

    Ant design date picker sample

--- index.js
+++ index.js
@@ -2,10 +2,29 @@ import './index.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { DatePicker, message } from 'antd'
 
 class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      date: ''
+    }
+  }
+  handleChange (date) {
+    if (date === null) {
+      date = ''
+    }
+    message.info('您选择的日期是: ' + date.toString())
+    this.setState({ date })
+  }
   render () {
-    return <div>Hello world</div>
+    return (
+      <div style={{ width: 400, margin: '100px auto' }}>
+        <DatePicker onChange={value => this.handleChange(value)} />
+        <div style={{ marginTop: 20 }}>当前日期：{this.state.date.toString()}</div>
+      </div>
+    )
   }
 }
 
