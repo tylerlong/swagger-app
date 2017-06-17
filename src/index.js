b9a0fea01eeb2c510ef4656ace093f7867c1cb43
Tyler Long <tyler4long@gmail.com>
2017-06-17 22:24:47 +0800

    Two text fields

--- src/index.js
+++ src/index.js
@@ -2,17 +2,19 @@ import './index.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { message, Input } from 'antd'
+import { message, Input, Form } from 'antd'
 import axios from 'axios'
 
 class App extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      name: ''
+      name: '',
+      version: ''
     }
 
-    this.handleChange = this.handleChange.bind(this)
+    this.handleChangeName = this.handleChangeName.bind(this)
+    this.handleChangeVersion = this.handleChangeVersion.bind(this)
   }
   componentDidMount () {
     axios.get('./sample.json').then(res => {
@@ -20,16 +22,34 @@ class App extends React.Component {
       message.success('state loaded')
     })
   }
-  handleChange (event) {
-    this.setState({name: event.target.value})
+  handleChangeName (event) {
+    this.setState({ name: event.target.value })
+  }
+  handleChangeVersion (event) {
+    this.setState({ version: event.target.value })
   }
   render () {
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 6 }
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 12 }
+      }
+    }
     return (
       <div>
-        <h1>{this.state.name}</h1>
-        <div style={{margin: '0 auto', width: '50%'}}>
-          <Input placeholder='Name' size='large' value={this.state.name} onChange={this.handleChange} />
-        </div>
+        <h1>{this.state.name} {this.state.version}</h1>
+        <Form>
+          <Form.Item {...formItemLayout} label='Name'>
+            <Input placeholder='Name' size='large' value={this.state.name} onChange={this.handleChangeName} />
+          </Form.Item>
+          <Form.Item {...formItemLayout} label='Version'>
+            <Input placeholder='Version' size='large' value={this.state.version} onChange={this.handleChangeVersion} />
+          </Form.Item>
+        </Form>
       </div>
     )
   }
