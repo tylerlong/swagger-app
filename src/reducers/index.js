9225e14f447f0b32cde1174b19853a410dd878c6
Tyler Long <tyler4long@gmail.com>
2017-06-24 22:30:30 +0800

    Move Model Up/Down

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -74,6 +74,22 @@ const reducer = (state = defaultState, action) => {
         R.over(R.lensPath(['pathParameters']), R.remove(activePathParameterIndex, 1)),
         R.over(R.lensPath(['metadata', 'activePathParameterIndex']), R.inc)
       )(state)
+    case 'MOVE_MODEL_UP':
+      const modelIndex = state.metadata.activeModelIndex
+      const model = state.models[modelIndex]
+      return R.pipe(
+        R.over(R.lensPath(['models']), R.insert(modelIndex - 1, model)),
+        R.over(R.lensPath(['models']), R.remove(modelIndex + 1, 1)),
+        R.over(R.lensPath(['metadata', 'activeModelIndex']), R.dec)
+      )(state)
+    case 'MOVE_MODEL_DOWN':
+      const activeModelIndex = state.metadata.activeModelIndex
+      const activeModel = state.models[activeModelIndex]
+      return R.pipe(
+        R.over(R.lensPath(['models']), R.insert(activeModelIndex + 2, activeModel)),
+        R.over(R.lensPath(['models']), R.remove(activeModelIndex, 1)),
+        R.over(R.lensPath(['metadata', 'activeModelIndex']), R.inc)
+      )(state)
     default:
       console.log(`Unknown action type: ${action.type}`)
       return state
