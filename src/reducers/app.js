397216135f407bcc049b98824f3fc4f3d9906dc6
Tyler Long <tyler4long@gmail.com>
2017-06-25 10:12:50 +0800

    Refactor code

--- src/reducers/app.js
+++ src/reducers/app.js
@@ -1,13 +1,39 @@
 import R from 'ramda'
 
-const reducer = (state, action) => {
+const defaultState = {
+  metadata: {
+    activeTabKey: 'info',
+    activePermissionIndex: 0,
+    activePathParameterIndex: 0,
+    activeModelIndex: 0
+  },
+  info: {
+    title: 'Example API',
+    version: '1.0',
+    description: 'Restful API for Example',
+    termsOfService: 'https://www.example.com/terms-of-service',
+    host: 'api.example.com',
+    basePath: '/',
+    schemes: 'https',
+    produces: 'application/json',
+    consumes: 'application/json'
+  },
+  permissions: [],
+  pathParameters: [],
+  paths: [],
+  models: []
+}
+
+const reducer = (state = defaultState, action) => {
   switch (action.type) {
     case 'SET_PROP':
       return R.set(R.lensPath(action.path), action.value, state)
     case 'SET_STATE':
       return action.state
     default:
-      console.log(`Unknown action type: ${action.type}`)
+      if (action.type !== '@@redux/INIT') { // When app starts, invoked by Redux
+        console.log(`Unknown action type: ${action.type}`)
+      }
       return state
   }
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,35 +1,11 @@
 import R from 'ramda'
 
+import appReducer from './app'
 import permissionsReducer from './permissions'
 import pathParametersReducer from './pathParameters'
 import modelsReducer from './models'
-import appReducer from './app'
 
-const defaultState = {
-  metadata: {
-    activeTabKey: 'info',
-    activePermissionIndex: 0,
-    activePathParameterIndex: 0,
-    activeModelIndex: 0
-  },
-  info: {
-    title: 'Example API',
-    version: '1.0',
-    description: 'Restful API for Example',
-    termsOfService: 'https://www.example.com/terms-of-service',
-    host: 'api.example.com',
-    basePath: '/',
-    schemes: 'https',
-    produces: 'application/json',
-    consumes: 'application/json'
-  },
-  permissions: [],
-  pathParameters: [],
-  paths: [],
-  models: []
-}
-
-const reducer = (state = defaultState, action) => {
+const reducer = (state, action) => {
   if (R.test(/PERMISSION/, action.type)) {
     return permissionsReducer(state, action)
   }
