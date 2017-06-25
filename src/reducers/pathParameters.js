7407254f261c5dcd6469dd8273f5e60f6d4f0a9d
Tyler Long <tyler4long@gmail.com>
2017-06-25 11:48:33 +0800

    Refactor code

--- src/reducers/pathParameters.js
+++ src/reducers/pathParameters.js
@@ -1,5 +1,7 @@
 import R from 'ramda'
 
+import { swap } from '../utils/reducers'
+
 const reducer = (state, action) => {
   switch (action.type) {
     case 'ADD_PATH_PARAMETER':
@@ -7,19 +9,13 @@ const reducer = (state, action) => {
     case 'DELETE_PATH_PARAMETER':
       return R.over(R.lensPath(['pathParameters']), R.remove(state.metadata.activePathParameterIndex, 1), state)
     case 'MOVE_PATH_PARAMETER_UP':
-      const pathParameterIndex = state.metadata.activePathParameterIndex
-      const pathParameter = state.pathParameters[pathParameterIndex]
       return R.pipe(
-        R.over(R.lensPath(['pathParameters']), R.insert(pathParameterIndex - 1, pathParameter)),
-        R.over(R.lensPath(['pathParameters']), R.remove(pathParameterIndex + 1, 1)),
+        R.over(R.lensPath(['pathParameters']), swap(state.metadata.activePathParameterIndex, state.metadata.activePathParameterIndex - 1)),
         R.over(R.lensPath(['metadata', 'activePathParameterIndex']), R.dec)
       )(state)
     case 'MOVE_PATH_PARAMETER_DOWN':
-      const activePathParameterIndex = state.metadata.activePathParameterIndex
-      const activePathParameter = state.pathParameters[activePathParameterIndex]
       return R.pipe(
-        R.over(R.lensPath(['pathParameters']), R.insert(activePathParameterIndex + 2, activePathParameter)),
-        R.over(R.lensPath(['pathParameters']), R.remove(activePathParameterIndex, 1)),
+        R.over(R.lensPath(['pathParameters']), swap(state.metadata.activePathParameterIndex, state.metadata.activePathParameterIndex + 1)),
         R.over(R.lensPath(['metadata', 'activePathParameterIndex']), R.inc)
       )(state)
     default:
