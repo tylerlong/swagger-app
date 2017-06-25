08cc36604fbad94baa9a587c01ee76d8f9cfbdb0
Tyler Long <tyler4long@gmail.com>
2017-06-25 11:44:10 +0800

    Refactor code

--- src/utils/reducers.js
+++ src/utils/reducers.js
@@ -1,14 +1,13 @@
 import R from 'ramda'
 
 export const swap = R.curry((index1, index2, list) => {
-  if (index1 > list.length - 1 || index2 > list.length - 1) {
-    return list
+  if (index1 < 0 || index2 < 0 || index1 > list.length - 1 || index2 > list.length - 1) {
+    return list // index of out bound
   }
   const value1 = list[index1]
   const value2 = list[index2]
-  const result = R.pipe(
+  return R.pipe(
     R.set(R.lensIndex(index1), value2),
     R.set(R.lensIndex(index2), value1)
   )(list)
-  return result
 })
