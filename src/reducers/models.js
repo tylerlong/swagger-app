36aa985d85fd84e99a73718eb7d041e5740dd6b5
Tyler Long <tyler4long@gmail.com>
2017-06-25 11:42:36 +0800

    Refactor code

--- src/reducers/models.js
+++ src/reducers/models.js
@@ -1,5 +1,7 @@
 import R from 'ramda'
 
+import { swap } from '../utils/reducers'
+
 const reducer = (state, action) => {
   switch (action.type) {
     case 'ADD_MODEL':
@@ -7,19 +9,13 @@ const reducer = (state, action) => {
     case 'DELETE_MODEL':
       return R.over(R.lensPath(['models']), R.remove(state.metadata.activeModelIndex, 1), state)
     case 'MOVE_MODEL_UP':
-      const modelIndex = state.metadata.activeModelIndex
-      const model = state.models[modelIndex]
       return R.pipe(
-        R.over(R.lensPath(['models']), R.insert(modelIndex - 1, model)),
-        R.over(R.lensPath(['models']), R.remove(modelIndex + 1, 1)),
+        R.over(R.lensPath(['models']), swap(state.metadata.activeModelIndex, state.metadata.activeModelIndex - 1)),
         R.over(R.lensPath(['metadata', 'activeModelIndex']), R.dec)
       )(state)
     case 'MOVE_MODEL_DOWN':
-      const activeModelIndex = state.metadata.activeModelIndex
-      const activeModel = state.models[activeModelIndex]
       return R.pipe(
-        R.over(R.lensPath(['models']), R.insert(activeModelIndex + 2, activeModel)),
-        R.over(R.lensPath(['models']), R.remove(activeModelIndex, 1)),
+        R.over(R.lensPath(['models']), swap(state.metadata.activeModelIndex, state.metadata.activeModelIndex + 1)),
         R.over(R.lensPath(['metadata', 'activeModelIndex']), R.inc)
       )(state)
     default:
