692149abada218ecfd523e51ff05ed74cc3aaec0
Tyler Long <tyler4long@gmail.com>
2017-06-26 08:57:24 +0800

    Refactor code

--- src/components/Info/FormItem.js
+++ src/components/Info/FormItem.js
@@ -8,11 +8,11 @@ import { formItemLayout } from '../../utils/components'
 
 class FormItem extends React.Component {
   render () {
-    const path = this.props.path
-    const value = this.props.info[path]
+    const { path, info, setProp } = this.props
+    const value = info[path]
     return (
       <Form.Item {...formItemLayout} label={path}>
-        <Input placeholder={path} size='large' value={value} onChange={(event) => { this.props.setProp(['info', path], event.target.value) }} />
+        <Input placeholder={path} size='large' value={value} onChange={(event) => { setProp(['info', path], event.target.value) }} />
       </Form.Item>
     )
   }
--- src/components/Models/FormItem.js
+++ src/components/Models/FormItem.js
@@ -8,17 +8,18 @@ import { formItemLayout } from '../../utils/components'
 
 class FormItem extends React.Component {
   render () {
-    const model = this.props.models[this.props.index]
+    const { models, index, setProp, moveModelUp, moveModelDown, deleteModel } = this.props
+    const model = models[index]
     return (
       <div>
         <Form.Item {...formItemLayout} label='Name'>
-          <Input placeholder='Name' size='large' value={model.name} onChange={(event) => { this.props.setProp(['models', this.props.index, 'name'], event.target.value) }} />
+          <Input placeholder='Name' size='large' value={model.name} onChange={(event) => { setProp(['models', index, 'name'], event.target.value) }} />
         </Form.Item>
         <div className='button-line'>
-          <Button disabled={this.props.index === 0} onClick={this.props.moveModelUp}>Move up <Icon type='arrow-up' /></Button>
-          <Button disabled={this.props.index === this.props.models.length - 1} onClick={this.props.moveModelDown}>Move down <Icon type='arrow-down' /></Button>
+          <Button disabled={index === 0} onClick={moveModelUp}>Move up <Icon type='arrow-up' /></Button>
+          <Button disabled={index === models.length - 1} onClick={moveModelDown}>Move down <Icon type='arrow-down' /></Button>
           <Popconfirm title='Are you sure to delete it?' okText='Yes' cancelText='No' onConfirm={(event) => {
-            this.props.deleteModel(this.props.index)
+            deleteModel(index)
           }}>
             <Button type='danger'>Delete</Button>
           </Popconfirm>
