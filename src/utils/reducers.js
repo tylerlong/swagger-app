ae599c6110f31e1f8564ec7aa916e7b8a9df7d99
Tyler Long <tyler4long@gmail.com>
2017-06-26 16:39:57 +0800

    Test alert

--- src/utils/reducers.js
+++ src/utils/reducers.js
@@ -12,4 +12,12 @@ export const swap = R.curry((index1, index2, list) => {
   )(list)
 })
 
-export const alert = R.curry((type, message) => R.over(R.lensPath(['metadata', 'alerts']), R.append({ type, message })))
+export const alert = R.curry((type, message) => {
+  if (R.complement(R.contains)(type, ['success', 'info', 'warning', 'error'])) {
+    return R.identity
+  }
+  if (R.either(R.complement(R.is(String)), R.isEmpty)(message)) {
+    return R.identity
+  }
+  return R.over(R.lensPath(['metadata', 'alerts']), R.append({ type, message }))
+})
--- src/utils/reducers.test.js
+++ src/utils/reducers.test.js
@@ -1,5 +1,5 @@
 /* eslint-env jest */
-const { swap } = require('./reducers')
+const { swap, alert } = require('./reducers')
 
 test('swap two elements in an array', () => {
   expect(swap(0, 2, [1, 2, 3])).toEqual([3, 2, 1])
@@ -9,4 +9,30 @@ test('swap two elements in an array', () => {
 test('swap index out of bound', () => {
   expect(swap(0, 3, [1, 2, 3])).toEqual([1, 2, 3])
   expect(swap(-1, 2, [1, 2, 3])).toEqual([1, 2, 3])
+})
+
+test('alert success/info/warning/error', () => {
+  const state = { metadata: { alerts: [] } }
+  expect(alert('success', 'Hello world')(state)).toEqual({ metadata: { alerts: [{ type: 'success', message: 'Hello world' }] } })
+  expect(alert('info', 'Hello world')(state)).toEqual({ metadata: { alerts: [{ type: 'info', message: 'Hello world' }] } })
+  expect(alert('warning', 'Hello world')(state)).toEqual({ metadata: { alerts: [{ type: 'warning', message: 'Hello world' }] } })
+  expect(alert('error', 'Hello world')(state)).toEqual({ metadata: { alerts: [{ type: 'error', message: 'Hello world' }] } })
+})
+
+test('alert unknown type', () => {
+  const state = { metadata: { alerts: [] } }
+  expect(alert('bug', 'Hello world')(state)).not.toEqual({ metadata: { alerts: [{ type: 'bug', message: 'Hello world' }] } })
+  expect(alert('bug', 'Hello world')(state)).toEqual(state)
+})
+
+test('alert invalid message', () => {
+  const state = { metadata: { alerts: [] } }
+  expect(alert('success', null)(state)).not.toEqual({ metadata: { alerts: [{ type: 'success', message: null }] } })
+  expect(alert('success', null)(state)).toEqual(state)
+  expect(alert('success', '')(state)).not.toEqual({ metadata: { alerts: [{ type: 'success', message: '' }] } })
+  expect(alert('success', '')(state)).toEqual(state)
+  expect(alert('success', undefined)(state)).not.toEqual({ metadata: { alerts: [{ type: 'success', message: undefined }] } })
+  expect(alert('success', undefined)(state)).toEqual(state)
+  expect(alert('success', ['Hello world'])(state)).not.toEqual({ metadata: { alerts: [{ type: 'success', message: ['Hello world'] }] } })
+  expect(alert('success', ['Hello world'])(state)).toEqual(state)
 })
