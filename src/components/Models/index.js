1ae793b4861bc6437d2722e4bca251c9c0a6247d
Tyler Long <tyler4long@gmail.com>
2017-06-26 09:23:23 +0800

    Refactor

--- src/components/Models/index.js
+++ src/components/Models/index.js
@@ -11,9 +11,6 @@ class Models extends React.Component {
     super(props)
     this.setActiveIndex = this.setActiveIndex.bind(this)
   }
-  activeKey () {
-    return `${this.props.models.length}-${this.props.metadata.activeModelIndex}`
-  }
   setActiveIndex (key) {
     let activeIndex = -1
     if (key) {
@@ -22,20 +19,21 @@ class Models extends React.Component {
     this.props.setProp(['metadata', 'activeModelIndex'], activeIndex)
   }
   render () {
+    const { models, metadata, addModel } = this.props
     return (
       <div>
         <h2>Models</h2>
-        <Collapse accordion activeKey={this.activeKey()} onChange={this.setActiveIndex}>
-          {this.props.models.map((model, index) => {
+        <Collapse accordion activeKey={`${models.length}-${metadata.activeModelIndex}`} onChange={this.setActiveIndex}>
+          {models.map((model, index) => {
             return (
-              <Collapse.Panel header={model.name} key={`${this.props.models.length}-${index}`}>
+              <Collapse.Panel header={model.name} key={`${models.length}-${index}`}>
                 <FormItem index={index} />
               </Collapse.Panel>
             )
           })}
         </Collapse>
         <div className='primary-button'>
-          <Button type='primary' size='large' onClick={this.props.addModel}>Add model</Button>
+          <Button type='primary' size='large' onClick={addModel}>Add model</Button>
         </div>
       </div>
     )
