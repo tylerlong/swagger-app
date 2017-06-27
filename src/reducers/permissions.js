20576bd3440d2b078c051c4259d42b07e735ebbc
Tyler Long <tyler4long@gmail.com>
2017-06-27 10:13:57 +0800

    Refactor code

--- src/reducers/permissions.js
+++ src/reducers/permissions.js
@@ -9,28 +9,28 @@ const reducer = (state, action) => {
         R.over(R.lensPath(['permissions']), R.append(R.omit(['type'], action))),
         R.set(R.lensPath(['metadata', 'activePermissionIndex']), state.permissions.length),
         alert('success', 'Permission added')
-      )(state)
+      )
     case 'DELETE_PERMISSION':
       return R.pipe(
         R.over(R.lensPath(['permissions']), R.remove(state.metadata.activePermissionIndex, 1)),
         R.set(R.lensPath(['metadata', 'activePermissionIndex']), -1),
         alert('success', 'Permission deleted')
-      )(state)
+      )
     case 'MOVE_PERMISSION_UP':
       return R.pipe(
         R.over(R.lensPath(['permissions']), swap(state.metadata.activePermissionIndex, state.metadata.activePermissionIndex - 1)),
         R.over(R.lensPath(['metadata', 'activePermissionIndex']), R.dec),
         alert('success', 'Permission moved up')
-      )(state)
+      )
     case 'MOVE_PERMISSION_DOWN':
       return R.pipe(
         R.over(R.lensPath(['permissions']), swap(state.metadata.activePermissionIndex, state.metadata.activePermissionIndex + 1)),
         R.over(R.lensPath(['metadata', 'activePermissionIndex']), R.inc),
         alert('success', 'Permission moved down')
-      )(state)
+      )
     default:
-      return alert('error', `Unknown action type: ${action.type}`)(state)
+      return alert('error', `Unknown action type: ${action.type}`)
   }
 }
 
-export default reducer
+export default (state, action) => reducer(state, action)(state)
