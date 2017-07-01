da2c6507aeb8ce5c283f42fe943bfb819ee8c42d
Tyler Long <tyler4long@gmail.com>
2017-07-01 20:49:49 +0800

    Refactor testing code

--- src/utils/reducers.test.js
+++ src/utils/reducers.test.js
@@ -1,38 +1,43 @@
 /* eslint-env jest */
 const { swap, alert } = require('./reducers')
 
-test('swap two elements in an array', () => {
-  expect(swap(0, 2, [1, 2, 3])).toEqual([3, 2, 1])
-  expect(swap(1, 0, [1, 2, 3])).toEqual([2, 1, 3])
+describe('test swap', () => {
+  const state = [1, 2, 3]
+
+  test('swap two elements in an array', () => {
+    expect(swap(0, 2, state)).toEqual([3, 2, 1])
+    expect(swap(1, 0, state)).toEqual([2, 1, 3])
+  })
+
+  test('swap index out of bound', () => {
+    expect(swap(0, 3, state)).toEqual([1, 2, 3])
+    expect(swap(-1, 2, state)).toEqual([1, 2, 3])
+  })
 })
 
-test('swap index out of bound', () => {
-  expect(swap(0, 3, [1, 2, 3])).toEqual([1, 2, 3])
-  expect(swap(-1, 2, [1, 2, 3])).toEqual([1, 2, 3])
-})
-
-test('alert success/info/warning/error', () => {
+describe('test alert', () => {
   const state = { metadata: { alerts: [] } }
-  expect(alert('success', 'Hello world')(state)).toEqual({ metadata: { alerts: [{ type: 'success', message: 'Hello world' }] } })
-  expect(alert('info', 'Hello world')(state)).toEqual({ metadata: { alerts: [{ type: 'info', message: 'Hello world' }] } })
-  expect(alert('warning', 'Hello world')(state)).toEqual({ metadata: { alerts: [{ type: 'warning', message: 'Hello world' }] } })
-  expect(alert('error', 'Hello world')(state)).toEqual({ metadata: { alerts: [{ type: 'error', message: 'Hello world' }] } })
-})
 
-test('alert unknown type', () => {
-  const state = { metadata: { alerts: [] } }
-  expect(alert('bug', 'Hello world')(state)).not.toEqual({ metadata: { alerts: [{ type: 'bug', message: 'Hello world' }] } })
-  expect(alert('bug', 'Hello world')(state)).toEqual(state)
-})
+  test('alert success/info/warning/error', () => {
+    expect(alert('success', 'Hello world')(state)).toEqual({ metadata: { alerts: [{ type: 'success', message: 'Hello world' }] } })
+    expect(alert('info', 'Hello world')(state)).toEqual({ metadata: { alerts: [{ type: 'info', message: 'Hello world' }] } })
+    expect(alert('warning', 'Hello world')(state)).toEqual({ metadata: { alerts: [{ type: 'warning', message: 'Hello world' }] } })
+    expect(alert('error', 'Hello world')(state)).toEqual({ metadata: { alerts: [{ type: 'error', message: 'Hello world' }] } })
+  })
 
-test('alert invalid message', () => {
-  const state = { metadata: { alerts: [] } }
-  expect(alert('success', null)(state)).not.toEqual({ metadata: { alerts: [{ type: 'success', message: null }] } })
-  expect(alert('success', null)(state)).toEqual(state)
-  expect(alert('success', '')(state)).not.toEqual({ metadata: { alerts: [{ type: 'success', message: '' }] } })
-  expect(alert('success', '')(state)).toEqual(state)
-  expect(alert('success', undefined)(state)).not.toEqual({ metadata: { alerts: [{ type: 'success', message: undefined }] } })
-  expect(alert('success', undefined)(state)).toEqual(state)
-  expect(alert('success', ['Hello world'])(state)).not.toEqual({ metadata: { alerts: [{ type: 'success', message: ['Hello world'] }] } })
-  expect(alert('success', ['Hello world'])(state)).toEqual(state)
+  test('alert unknown type', () => {
+    expect(alert('bug', 'Hello world')(state)).not.toEqual({ metadata: { alerts: [{ type: 'bug', message: 'Hello world' }] } })
+    expect(alert('bug', 'Hello world')(state)).toEqual(state)
+  })
+
+  test('alert invalid message', () => {
+    expect(alert('success', null)(state)).not.toEqual({ metadata: { alerts: [{ type: 'success', message: null }] } })
+    expect(alert('success', null)(state)).toEqual(state)
+    expect(alert('success', '')(state)).not.toEqual({ metadata: { alerts: [{ type: 'success', message: '' }] } })
+    expect(alert('success', '')(state)).toEqual(state)
+    expect(alert('success', undefined)(state)).not.toEqual({ metadata: { alerts: [{ type: 'success', message: undefined }] } })
+    expect(alert('success', undefined)(state)).toEqual(state)
+    expect(alert('success', ['Hello world'])(state)).not.toEqual({ metadata: { alerts: [{ type: 'success', message: ['Hello world'] }] } })
+    expect(alert('success', ['Hello world'])(state)).toEqual(state)
+  })
 })
