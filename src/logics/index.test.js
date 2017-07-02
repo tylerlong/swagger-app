0b8393424cdb37fd714f2834415d94bb8a8ec9b0
Tyler Long <tyler4long@gmail.com>
2017-07-02 16:57:26 +0800

    First successful async HTTP test

--- src/logics/index.test.js
+++ src/logics/index.test.js
@@ -1,11 +1,18 @@
 /* eslint-env jest */
-// import nock from 'nock'
+import nock from 'nock'
+import axios from 'axios'
 import R from 'ramda'
 import { createMockStore } from 'redux-logic-test'
+import httpAdapter from 'axios/lib/adapters/http'
 
 import reducer from '../reducers'
 import logic from './index'
-import { setState } from '../actions'
+import { loadState, setState } from '../actions'
+
+const host = 'http://localhost'
+
+axios.defaults.host = host
+axios.defaults.adapter = httpAdapter
 
 const store = createMockStore({
   initialState: { message: 'hello' },
@@ -15,7 +22,15 @@ const store = createMockStore({
 
 test('setState', () => {
   store.dispatch(setState({ message: 'world' }))
-  return store.whenComplete(() => { // must return this, otherwise jest don't know it failed
+  return store.whenComplete(() => { // must `return`, otherwise jest won't report failure
     expect(R.omit('metadata', store.getState())).toEqual({ message: 'world' })
+  })
+})
+
+test('loadState', () => {
+  nock(host).get('/state.json').reply(200, { message: 'Hello world' })
+  store.dispatch(loadState())
+  return store.whenComplete(() => {
+    expect(R.omit('metadata', store.getState())).toEqual({ message: 'Hello world' })
   })
 })
