9dd248aab5efbeddad24849e85d896b63f4a330e
Tyler Long <tyler4long@gmail.com>
2017-07-10 10:56:09 +0800

    Refactor permission test

--- test/permissions.spec.js
+++ test/permissions.spec.js
@@ -9,53 +9,70 @@ import Permissions from '../src/components/Permissions'
 import store from './store'
 import { setState } from '../src/actions'
 
-const initialState = { permissions: [
-  {
-    'createdAt': 1498875025725,
-    'name': 'EditAccounts',
-    'description': 'Viewing and updating user account info (including name, business name, address and phone number/account number)'
-  },
-  {
-    'createdAt': 1498875008388,
-    'name': 'Accounts',
-    'description': 'Managing accounts: creating new accounts, viewing and updating account information, deleting existing accounts'
-  },
-  {
-    'createdAt': 1498875020815,
-    'name': 'Contacts',
-    'description': 'Creating, viewing, editing and deleting user personal contacts'
-  }
-]}
+const initialState = {
+  permissions: [
+    {
+      'createdAt': 1498875025725,
+      'name': 'EditAccounts',
+      'description': 'Viewing and updating user account info (including name, business name, address and phone number/account number)'
+    },
+    {
+      'createdAt': 1498875008388,
+      'name': 'Accounts',
+      'description': 'Managing accounts: creating new accounts, viewing and updating account information, deleting existing accounts'
+    },
+    {
+      'createdAt': 1498875020815,
+      'name': 'Contacts',
+      'description': 'Creating, viewing, editing and deleting user personal contacts'
+    }
+  ]
+}
 
 beforeEach(() => {
   store.dispatch(setState(initialState))
   store.resetActions()
 })
 
-test('test permissions list', () => {
-  const wrapper = mount(
-    <Provider store={store}>
-      <Permissions />
-    </Provider>
-  )
-  expect(store.getState().permissions.length).toEqual(3)
-  expect(wrapper.find('button.ant-btn-primary').length).toEqual(1)
-  expect(toJson(wrapper)).toMatchSnapshot()
+describe('test permission', () => {
+  test('permissions list', () => {
+    const wrapper = mount(
+      <Provider store={store}>
+        <Permissions />
+      </Provider>
+    )
+    expect(store.getState().permissions.length).toEqual(3)
+    expect(wrapper.find('button.ant-btn-primary').length).toEqual(1)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
 
-  // add permission
-  wrapper.find('button.ant-btn-primary').simulate('click')
-  expect(store.getState().permissions.length).toEqual(4)
-  expect(toJson(wrapper)).toMatchSnapshot()
+  test('add permission', () => {
+    const wrapper = mount(
+      <Provider store={store}>
+        <Permissions />
+      </Provider>
+    )
+    wrapper.find('button.ant-btn-primary').simulate('click')
+    expect(store.getState().permissions.length).toEqual(4)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
 
-  // view permission
-  wrapper.find('div.ant-collapse-header').first().simulate('click')
-  expect(wrapper.find(Popconfirm).length).toEqual(1)
-  expect(store.getState().permissions.length).toEqual(4)
-  expect(toJson(wrapper)).toMatchSnapshot()
+  test('delete permission', () => {
+    const wrapper = mount(
+      <Provider store={store}>
+        <Permissions />
+      </Provider>
+    )
+    // view permission
+    wrapper.find('div.ant-collapse-header').first().simulate('click')
+    expect(wrapper.find(Popconfirm).length).toEqual(1)
+    expect(store.getState().permissions.length).toEqual(3)
+    expect(toJson(wrapper)).toMatchSnapshot()
 
-  // delete permission
-  wrapper.find(Popconfirm).props().onConfirm()
-  expect(store.getState().permissions.length).toEqual(3)
-  expect(wrapper.find(Popconfirm).length).toEqual(0)
-  expect(toJson(wrapper)).toMatchSnapshot()
+    // delete permission
+    wrapper.find(Popconfirm).props().onConfirm()
+    expect(store.getState().permissions.length).toEqual(2)
+    expect(wrapper.find(Popconfirm).length).toEqual(0)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
 })
