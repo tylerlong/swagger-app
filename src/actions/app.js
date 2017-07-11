bb37b1b956bfe1c66974c03fce9bef62643c463d
Tyler Long <tyler4long@gmail.com>
2017-07-11 13:31:11 +0800

    Write some flow

--- src/actions/app.js
+++ src/actions/app.js
@@ -1,5 +1,7 @@
-export const setProp = (path, value) => ({ type: 'SET_PROP', path, value })
+// @flow
+
+export const setProp = (path: string[], value: any) => ({ type: 'SET_PROP', path, value })
 
 export const loadState = () => ({ type: 'LOAD_STATE' })
 
-export const setState = (state) => ({ type: 'SET_STATE', state })
+export const setState = (state: any) => ({ type: 'SET_STATE', state })
