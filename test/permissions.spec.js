83b6b43b11d64301a5389aa8e169c8559e978129
Tyler Long <tyler4long@gmail.com>
2017-07-12 20:47:29 +0800

    Test edit permission name

--- test/permissions.spec.js
+++ test/permissions.spec.js
@@ -7,6 +7,7 @@ import Permissions from '../src/components/Permissions'
 import store from './store'
 import { getWrapper } from './shared'
 import state from '../dist/state.json'
+import FormItem from '../src/components/Permissions/FormItem'
 
 let wrapper = null
 beforeEach(() => {
@@ -37,6 +38,22 @@ describe('test permission', () => {
     wrapper.find(Popconfirm).props().onConfirm()
     expect(store.getState().permissions.length).toEqual(2)
     expect(wrapper.find(Popconfirm).length).toEqual(0)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+
+  test('edit permission name', () => {
+    // view permission
+    wrapper.find('div.ant-collapse-header').first().simulate('click')
+    expect(wrapper.find(Popconfirm).length).toEqual(1)
+    expect(store.getState().permissions.length).toEqual(3)
+    expect(toJson(wrapper)).toMatchSnapshot()
+
+    // update permission name
+    const index = wrapper.find(FormItem).first().props().index
+    wrapper.find('input').first().simulate('change', { target: { value: 'Hello' } })
+    expect(store.getState().permissions[index].name).toEqual('Hello')
+    wrapper.find('input').first().simulate('change', { target: { value: 'World' } })
+    expect(store.getState().permissions[index].name).toEqual('World')
     expect(toJson(wrapper)).toMatchSnapshot()
   })
 })
