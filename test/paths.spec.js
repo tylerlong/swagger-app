c3414714be4f0b3819158c991ce11ba094f68fc5
Tyler Long <tyler4long@gmail.com>
2017-07-12 11:15:11 +0800

    Refactor paths test

--- test/paths.spec.js
+++ test/paths.spec.js
@@ -1,40 +1,18 @@
 /* eslint-env jest */
+import R from 'ramda'
 import toJson from 'enzyme-to-json'
 
 import Paths from '../src/components/Paths'
 import { getWrapper } from './shared'
+import state from '../dist/state.json'
 
-const state = {
-  'paths': [
-    {
-      'createdAt': 1498875254374,
-      'path': '/restapi',
-      'methods': [
-        {
-          'since': '1.0.2',
-          'apiGroup': 'Light',
-          'permissions': [],
-          'batch': false,
-          'visibility': 'public',
-          'status': 'normal',
-          'method': 'get',
-          'description': 'Get Server Info',
-          'tags': [
-            'API Versions'
-          ],
-          'parameters': {},
-          'request': {},
-          'response': {},
-          'examples': []
-        }
-      ]
-    }
-  ]
-}
+let wrapper = null
+beforeEach(() => {
+  wrapper = getWrapper(Paths, R.pick(['paths'], state))
+})
 
 describe('test paths', () => {
   test('view paths', () => {
-    const wrapper = getWrapper(Paths, state)
     expect(wrapper.find('.ant-collapse-item').length).toEqual(1)
     expect(toJson(wrapper)).toMatchSnapshot()
   })
