6d9fcb06a63570748bc45ed0fa98b6866afb26cb
Tyler Long <tyler4long@gmail.com>
2017-07-12 21:12:03 +0800

    Test deletePath

--- test/paths.spec.js
+++ test/paths.spec.js
@@ -1,6 +1,7 @@
 /* eslint-env jest */
 import R from 'ramda'
 import toJson from 'enzyme-to-json'
+import { Popconfirm } from 'antd'
 
 import Paths from '../src/components/Paths'
 import { getWrapper } from './shared'
@@ -15,7 +16,8 @@ beforeEach(() => {
 
 describe('test paths', () => {
   test('view paths', () => {
-    expect(wrapper.find('.ant-collapse-item').length).toEqual(1)
+    const count = store.getState().paths.length
+    expect(wrapper.find('.ant-collapse-item').length).toEqual(count)
     expect(toJson(wrapper)).toMatchSnapshot()
   })
 
@@ -23,6 +25,16 @@ describe('test paths', () => {
     const count = store.getState().paths.length
     wrapper.find('button.ant-btn-primary').simulate('click')
     expect(store.getState().paths.length).toEqual(count + 1)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+
+  test('delete path', () => {
+    // delete path
+    wrapper.find('div.ant-collapse-header').first().simulate('click')
+    const count = store.getState().paths.length
+    wrapper.find(Popconfirm).props().onConfirm()
+    expect(store.getState().paths.length).toEqual(count - 1)
+    expect(wrapper.find(Popconfirm).length).toEqual(0)
     expect(toJson(wrapper)).toMatchSnapshot()
   })
 
