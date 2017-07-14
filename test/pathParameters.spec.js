e3c014d2c68f2aad33d7250919051f298501b0ef
Tyler Long <tyler4long@gmail.com>
2017-07-14 14:17:42 +0800

    Refactor PathParameters testing code

--- test/pathParameters.spec.js
+++ test/pathParameters.spec.js
@@ -4,47 +4,45 @@ import toJson from 'enzyme-to-json'
 import { Popconfirm } from 'antd'
 
 import PathParameters from '../src/components/PathParameters'
+import FormItem from '../src/components/PathParameters/FormItem'
 import store from './store'
 import { getWrapper } from './shared'
 import state from '../dist/state.json'
-import FormItem from '../src/components/PathParameters/FormItem'
 
 let wrapper = null
 beforeEach(() => {
   wrapper = getWrapper(PathParameters, R.pick(['pathParameters'], state))
 })
+const count = state.pathParameters.length
+const getCount = () => store.getState().pathParameters.length
+const getModel = (index) => store.getState().pathParameters[index]
 
 describe('test pathParameters', () => {
   test('pathParameters list', () => {
-    expect(store.getState().pathParameters.length).toEqual(4)
-    expect(wrapper.find('button.ant-btn-primary').length).toEqual(1)
+    expect(wrapper.find('.ant-collapse-item').length).toEqual(count)
     expect(toJson(wrapper)).toMatchSnapshot()
   })
 
   test('add pathParameter', () => {
     wrapper.find('button.ant-btn-primary').simulate('click')
-    expect(store.getState().pathParameters.length).toEqual(5)
+    expect(getCount()).toEqual(count + 1)
     expect(toJson(wrapper)).toMatchSnapshot()
   })
 
   test('delete pathParameter', () => {
-    // delete pathParameter
     wrapper.find('div.ant-collapse-header').first().simulate('click')
-    const count = store.getState().pathParameters.length
     wrapper.find(Popconfirm).props().onConfirm()
-    expect(store.getState().pathParameters.length).toEqual(count - 1)
-    expect(wrapper.find(Popconfirm).length).toEqual(0)
+    expect(getCount()).toEqual(count - 1)
     expect(toJson(wrapper)).toMatchSnapshot()
   })
 
-  test('edit pathParameter name', () => {
-    // update pathParameter name
+  test('update pathParameter name', () => {
     wrapper.find('div.ant-collapse-header').first().simulate('click')
     const index = wrapper.find(FormItem).first().props().index
     wrapper.find('input').first().simulate('change', { target: { value: 'Hello' } })
-    expect(store.getState().pathParameters[index].name).toEqual('Hello')
+    expect(getModel(index).name).toEqual('Hello')
     wrapper.find('input').first().simulate('change', { target: { value: 'World' } })
-    expect(store.getState().pathParameters[index].name).toEqual('World')
+    expect(getModel(index).name).toEqual('World')
     expect(toJson(wrapper)).toMatchSnapshot()
   })
 })
