24ad299ac36e1cbfa4b55ac0c39fdb528c36a9b6
Tyler Long <tyler4long@gmail.com>
2017-07-14 11:19:05 +0800

    Refactor PathParameters

--- src/components/PathParameters/FormItem.js
+++ src/components/PathParameters/FormItem.js
@@ -9,23 +9,23 @@ import { formItemLayout } from '../../utils'
 class FormItem extends React.Component {
   render () {
     console.log(`render PathParameters.FormItem`)
-    const { index, pathParameter, setProp, deletePathParameter } = this.props
+    const { pathParameter, setProp, deletePathParameter } = this.props
     if (!pathParameter) {
       return null
     }
     return (
       <div>
-        <Popconfirm title={`Are you sure to delete path parameter "${pathParameter.name}"?`} okText='Yes' cancelText='No' onConfirm={() => deletePathParameter(index)}>
+        <Popconfirm title={`Are you sure to delete path parameter "${pathParameter.name}"?`} okText='Yes' cancelText='No' onConfirm={deletePathParameter}>
           <Button type='danger'><Icon type='arrow-up' />Delete</Button>
         </Popconfirm>
         <Form.Item {...formItemLayout} label='Name'>
-          <Input placeholder='Name' size='large' value={pathParameter.name} onChange={(event) => { setProp(['pathParameters', index, 'name'], event.target.value) }} />
+          <Input placeholder='Name' size='large' value={pathParameter.name} onChange={(event) => { setProp('name', event.target.value) }} />
         </Form.Item>
         <Form.Item {...formItemLayout} label='Description'>
-          <Input placeholder='Description' size='large' value={pathParameter.description} onChange={(event) => { setProp(['pathParameters', index, 'description'], event.target.value) }} />
+          <Input placeholder='Description' size='large' value={pathParameter.description} onChange={(event) => { setProp('description', event.target.value) }} />
         </Form.Item>
         <Form.Item {...formItemLayout} label='Enum'>
-          <Input placeholder='Enum values separated by commas' size='large' defaultValue={R.join(', ', pathParameter.enum)} onChange={(event) => { setProp(['pathParameters', index, 'enum'], R.pipe(R.split(','), R.map(R.trim), R.reject(R.equals('')))(event.target.value)) }} />
+          <Input placeholder='Enum values separated by commas' size='large' defaultValue={R.join(', ', pathParameter.enum)} onChange={(event) => { setProp('enum', R.pipe(R.split(','), R.map(R.trim), R.reject(R.equals('')))(event.target.value)) }} />
         </Form.Item>
       </div>
     )
@@ -33,4 +33,8 @@ class FormItem extends React.Component {
 }
 
 const mapStateToProps = ({ pathParameters }, { index }) => ({ pathParameter: pathParameters[index] })
-export default connect(mapStateToProps, { setProp, deletePathParameter })(FormItem)
+const mapDispathToProps = (dispatch, { index }) => ({
+  deletePathParameter: () => dispatch(deletePathParameter(index)),
+  setProp: (key, value) => dispatch(setProp(['pathParameters', index, key], value))
+})
+export default connect(mapStateToProps, mapDispathToProps)(FormItem)
--- test/pathParameters.spec.js
+++ test/pathParameters.spec.js
@@ -7,6 +7,7 @@ import PathParameters from '../src/components/PathParameters'
 import store from './store'
 import { getWrapper } from './shared'
 import state from '../dist/state.json'
+import FormItem from '../src/components/PathParameters/FormItem'
 
 let wrapper = null
 beforeEach(() => {
@@ -27,16 +28,23 @@ describe('test pathParameters', () => {
   })
 
   test('delete pathParameter', () => {
-    // view pathParameter
-    wrapper.find('div.ant-collapse-header').first().simulate('click')
-    expect(wrapper.find(Popconfirm).length).toEqual(1)
-    expect(store.getState().pathParameters.length).toEqual(4)
-    expect(toJson(wrapper)).toMatchSnapshot()
-
     // delete pathParameter
+    wrapper.find('div.ant-collapse-header').first().simulate('click')
+    const count = store.getState().pathParameters.length
     wrapper.find(Popconfirm).props().onConfirm()
-    expect(store.getState().pathParameters.length).toEqual(3)
+    expect(store.getState().pathParameters.length).toEqual(count - 1)
     expect(wrapper.find(Popconfirm).length).toEqual(0)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+
+  test('edit pathParameter name', () => {
+    // update pathParameter name
+    wrapper.find('div.ant-collapse-header').first().simulate('click')
+    const index = wrapper.find(FormItem).first().props().index
+    wrapper.find('input').first().simulate('change', { target: { value: 'Hello' } })
+    expect(store.getState().pathParameters[index].name).toEqual('Hello')
+    wrapper.find('input').first().simulate('change', { target: { value: 'World' } })
+    expect(store.getState().pathParameters[index].name).toEqual('World')
     expect(toJson(wrapper)).toMatchSnapshot()
   })
 })
