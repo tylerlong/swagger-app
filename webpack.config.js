f4b82a10f60277334ee2086ca847071716bd36b2
Tyler Long <tyler4long@gmail.com>
2017-09-13 17:33:28 +0800

    Refactor webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,6 @@ import path from 'path'
 import ExtractTextPlugin from 'extract-text-webpack-plugin'
 import WriteFilePlugin from 'write-file-webpack-plugin'
 
-const cssRule = {
-  test: /\.css$/,
-  use: ExtractTextPlugin.extract({
-    fallback: 'style-loader',
-    use: 'css-loader'
-  })
-}
-
-const jsRule = {
-  test: /\.js$/,
-  exclude: /node_modules/,
-  use: {
-    loader: 'babel-loader',
-    options: {
-      presets: [
-        'react'
-      ],
-      plugins: [
-        ['import', { libraryName: 'antd', style: 'css' }]
-      ]
-    }
-  }
-}
-
-const electronRule = {
-  test: /\.js$/,
-  use: {
-    loader: 'babel-loader'
-  }
-}
-
 const rendererConfig = {
   target: 'web',
   entry: {
@@ -42,7 +11,31 @@ const rendererConfig = {
     path: path.join(__dirname, 'dist'),
     filename: '[name].bundle.js'
   },
-  module: { rules: [jsRule, cssRule] },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: 'css-loader'
+      })
+    },
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            'react'
+          ],
+          plugins: [
+            ['import', { libraryName: 'antd', style: 'css' }]
+          ]
+        }
+      }
+    }
+    ]
+  },
   plugins: [
     new ExtractTextPlugin('[name].bundle.css'),
     new WriteFilePlugin()
@@ -59,12 +52,16 @@ const mainConfig = {
     filename: 'electron.bundle.js',
     path: path.join(__dirname, 'dist')
   },
-  node: { // Don't mock the following:
-    __dirname: false,
-    __filename: false
+  node: {
+    __dirname: false // https://github.com/webpack/webpack/issues/2010#issuecomment-181256611
   },
   module: {
-    rules: [electronRule]
+    rules: [{
+      test: /\.js$/,
+      use: {
+        loader: 'babel-loader'
+      }
+    }]
   }
 }
 
