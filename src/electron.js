678b024006848b86d0308dbc164e203ad23f3932
Tyler Long <tyler4long@gmail.com>
2017-09-13 16:33:47 +0800

    Add electron

---
+++ src/electron.js
@@ -0,0 +1,26 @@
+import { app, BrowserWindow } from 'electron'
+import path from 'path'
+
+let browserWindow = null
+
+const createWindow = () => {
+  browserWindow = new BrowserWindow({ width: 1024, height: 768 })
+  browserWindow.loadURL(path.join('file://', __dirname, 'index.html'))
+  browserWindow.on('closed', () => {
+    browserWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (browserWindow === null) {
+    createWindow()
+  }
+})
--- webpack.config.js
+++ webpack.config.js
@@ -2,33 +2,48 @@ import path from 'path'
 import ExtractTextPlugin from 'extract-text-webpack-plugin'
 import WriteFilePlugin from 'write-file-webpack-plugin'
 
-const rules = [
-  {
-    test: /\.css$/,
-    use: ExtractTextPlugin.extract({
-      fallback: 'style-loader',
-      use: 'css-loader'
-    })
-  },
-  {
-    test: /\.js$/,
-    exclude: /node_modules/,
-    use: {
-      loader: 'babel-loader',
-      options: {
-        presets: [
-          ['env', { targets: { browsers: ['last 2 Chrome versions'] } }],
-          'react'
-        ],
-        plugins: [
-          ['import', { libraryName: 'antd', style: 'css' }]
-        ]
-      }
+const cssRule = {
+  test: /\.css$/,
+  use: ExtractTextPlugin.extract({
+    fallback: 'style-loader',
+    use: 'css-loader'
+  })
+}
+
+const jsRule = {
+  test: /\.js$/,
+  exclude: /node_modules/,
+  use: {
+    loader: 'babel-loader',
+    options: {
+      presets: [
+        ['env', { targets: { browsers: ['last 2 Chrome versions'] } }],
+        'react'
+      ],
+      plugins: [
+        ['import', { libraryName: 'antd', style: 'css' }]
+      ]
     }
   }
-]
+}
 
-const config = {
+const electronRule = {
+  test: /\.js$/,
+  use: {
+    loader: 'babel-loader',
+    options: {
+      presets: [
+        ['env', {
+          'targets': {
+            'electron': '1.7'
+          }
+        }]
+      ]
+    }
+  }
+}
+
+const rendererConfig = {
   target: 'web',
   entry: {
     index: './src/index.js'
@@ -37,7 +52,7 @@ const config = {
     path: path.join(__dirname, 'dist'),
     filename: '[name].bundle.js'
   },
-  module: { rules },
+  module: { rules: [jsRule, cssRule] },
   plugins: [
     new ExtractTextPlugin('[name].bundle.css'),
     new WriteFilePlugin()
@@ -47,4 +62,20 @@ const config = {
   }
 }
 
-export default [config]
+const mainConfig = {
+  target: 'electron-main',
+  entry: './src/electron.js',
+  output: {
+    filename: 'electron.bundle.js',
+    path: path.join(__dirname, 'dist')
+  },
+  node: { // Don't mock the following:
+    __dirname: false,
+    __filename: false
+  },
+  module: {
+    rules: [electronRule]
+  }
+}
+
+export default [rendererConfig, mainConfig]
