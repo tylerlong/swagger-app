0e328f0ec7c22c7d696eb417a06829f00abc1416
Tyler Long <tyler4long@gmail.com>
2017-09-14 19:01:22 +0800

    electron open file

--- src/electron/preload.js
+++ src/electron/preload.js
@@ -1,5 +1,7 @@
 import electron from 'electron'
+import fs from 'fs'
 
 process.once('loaded', () => {
   global.electron = electron.remote
+  global.fs = fs
 })
--- src/web/logics/index.js
+++ src/web/logics/index.js
@@ -1,16 +1,19 @@
 import { createLogic } from 'redux-logic'
 import axios from 'axios'
 
-import { setState, showAlert } from '../actions'
+import { setState } from '../actions'
 
 const loadStateLogic = createLogic({
   type: 'LOAD_STATE',
   latest: true,
   async process ({ getState, action }, dispatch, done) {
     if (global.electron) { // electron
-      global.electron.dialog.showOpenDialog({ properties: ['openFile'] })
-      dispatch(showAlert('success', 'file opened'))
-      window.location = window.location.href.split('#')[0] + '#/edit'
+      const filesOpened = global.electron.dialog.showOpenDialog({ properties: ['openFile'] })
+      if (filesOpened) {
+        const data = global.fs.readFileSync(filesOpened[0], 'utf-8')
+        dispatch(setState(JSON.parse(data)))
+        window.location = window.location.href.split('#')[0] + '#/edit'
+      }
     } else { // browser
       const res = await axios.get('/state.json')
       dispatch(setState(res.data))
