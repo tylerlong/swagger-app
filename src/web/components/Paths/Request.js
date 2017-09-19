36a749c0cdcf3f6a8faecccfffceda2282223111
Tyler Long <tyler4long@gmail.com>
2017-09-19 22:43:36 +0800

    Refactor

--- src/web/components/Paths/Request.js
+++ src/web/components/Paths/Request.js
@@ -8,20 +8,20 @@ import { subFormItemLayout } from '../../utils'
 class Request extends React.Component {
   render () {
     console.log(`render Paths.Request`)
-    const { prop, setProp, deletePathRequest } = this.props
-    if (!prop) {
+    const { request, setProp, deletePathRequest } = this.props
+    if (!request) {
       return null
     }
     return (
       <div>
-        <Popconfirm title={`Are you sure to delete request "${prop.name}"?`} okText='Yes' cancelText='No' onConfirm={deletePathRequest}>
+        <Popconfirm title={`Are you sure to delete request "${request.name}"?`} okText='Yes' cancelText='No' onConfirm={deletePathRequest}>
           <Button type='danger'><Icon type='arrow-up' />Delete</Button>
         </Popconfirm>
         <Form.Item {...subFormItemLayout} label='Name'>
-          <Input placeholder='Name' size='large' value={prop.name} onChange={(event) => { setProp('name', event.target.value) }} />
+          <Input placeholder='Name' size='large' value={request.name} onChange={(event) => { setProp('name', event.target.value) }} />
         </Form.Item>
         <Form.Item {...subFormItemLayout} label='Method'>
-          <Select style={{ width: 120 }} value={prop.method} onChange={(value) => { setProp('method', value) }}>
+          <Select style={{ width: 120 }} value={request.method} onChange={(value) => { setProp('method', value) }}>
             <Select.Option value='GET'>GET</Select.Option>
             <Select.Option value='POST'>POST</Select.Option>
             <Select.Option value='PUT'>PUT</Select.Option>
@@ -29,14 +29,14 @@ class Request extends React.Component {
           </Select>
         </Form.Item>
         <Form.Item {...subFormItemLayout} label='Description'>
-          <Input placeholder='Description' size='large' value={prop.description} onChange={(event) => { setProp('description', event.target.value) }} />
+          <Input placeholder='Description' size='large' value={request.description} onChange={(event) => { setProp('description', event.target.value) }} />
         </Form.Item>
       </div>
     )
   }
 }
 
-const mapStateToProps = ({ paths }, { index1, index2 }) => ({ prop: paths[index1].requests[index2] })
+const mapStateToProps = ({ paths }, { index1, index2 }) => ({ request: paths[index1].requests[index2] })
 const mapDispatchToProps = (dispatch, { index1, index2 }) => ({
   setProp: (key, value) => dispatch(setProp(['paths', index1, 'requests', index2, key], value)),
   deletePathRequest: () => dispatch(deleteFromArray('paths', index1, 'requests', index2))
