f3b89b400e3500e18b28c7570b931c318c52cdfe
Tyler Long <tyler4long@gmail.com>
2017-09-20 15:52:07 +0800

    Refactor code

--- src/web/containers/Models/index.js
+++ src/web/containers/Models/index.js
@@ -5,11 +5,11 @@ import { addModel } from '../../actions'
 import Models from '../../components/Models'
 import { orderBy } from '../../utils'
 
-const mapStateToProps = ({ models }) => {
-  let mappedModels = models
-  mappedModels = R.addIndex(R.map)(({ name, createdAt }, index) => ({ index, name, createdAt }), mappedModels)
-  mappedModels = orderBy(R.prop('name'), mappedModels)
-  return { models: mappedModels }
-}
+const mapStateToProps = ({ models }) => ({
+  models: R.pipe(
+    R.addIndex(R.map)(({ name, createdAt }, index) => ({ index, name, createdAt })),
+    orderBy(R.prop('name'))
+  )(models)
+})
 const mapDispatchToProps = { addModel }
 export default connect(mapStateToProps, mapDispatchToProps)(Models)
