6bb0d1f3dc39a49cd930158debcebeee28efd97a
Tyler Long <tyler4long@gmail.com>
2017-09-24 11:14:31 +0800

    createSelector default value to avoid undefined

--- src/web/containers/Paths/Path.js
+++ src/web/containers/Paths/Path.js
@@ -9,7 +9,7 @@ import { TextField, DeleteButton, AddButton } from '../../components/Common'
 const requestsSelector = createSelector(
   (state, props) => props.path,
   (state, props) => R.path(props.path.concat('requests'), state),
-  (path, requests) => R.pipe(
+  (path, requests = []) => R.pipe(
     R.addIndex(R.map)(({ name, createdAt }, index) => ({ path: path.concat(['requests', index]), label: name, createdAt })),
     R.sortBy(R.prop('createdAt'))
   )(requests)
