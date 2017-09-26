ef5873229b59d653f1cacaa62557cdc2c4367877
Tyler Long <tyler4long@gmail.com>
2017-09-26 17:04:14 +0800

    CenterPanel.propTypes

--- src/web/components/Common/CenterPanel.js
+++ src/web/components/Common/CenterPanel.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import { Row, Col } from 'antd'
+import PropTypes from 'prop-types'
 
 class CenterPanel extends React.Component {
   render () {
-    return <Row type='flex' justify='center'>
-      <Col xs={24} sm={22}>
-        {this.props.children}
-      </Col>
-    </Row>
+    return (
+      <Row type='flex' justify='center'>
+        <Col xs={24} sm={22}>
+          {this.props.children}
+        </Col>
+      </Row>
+    )
   }
+}
+
+CenterPanel.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.element.isRequired,
+    PropTypes.arrayOf(PropTypes.element.isRequired).isRequired
+  ]).isRequired
 }
 
 export default CenterPanel
