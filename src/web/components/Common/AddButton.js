f4d3cd218a65258cc3ad9d3530fbcbde386a5405
Tyler Long <tyler4long@gmail.com>
2017-09-27 00:34:01 +0800

    Add indefinite articles

--- src/web/components/Common/AddButton.js
+++ src/web/components/Common/AddButton.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Button, Icon } from 'antd'
 import PropTypes from 'prop-types'
+import a from 'indefinite'
 
 class AddButton extends React.Component {
   render () {
     console.log('render AddButton')
     const { name, add } = this.props
-    return <Button type='primary' onClick={add}><Icon type='plus' />Add {name}</Button>
+    return <Button type='primary' onClick={add}><Icon type='plus' />Add {a(name)}</Button>
   }
 }
 
