f14188377b8499cb44c757bde34bad2a36e89e4f
Tyler Long <tyler4long@gmail.com>
2017-10-18 17:21:05 +0800

    First version of "Swagger JSON"

--- src/web/logics/app.js
+++ src/web/logics/app.js
@@ -1,7 +1,18 @@
 import { createLogic } from 'redux-logic'
 import axios from 'axios'
+import moment from 'moment'
 
 import { setState } from '../actions'
+
+const downloadFile = (filename, text) => {
+  const element = document.createElement('a')
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text))
+  element.setAttribute('download', filename)
+  element.style.display = 'none'
+  document.body.appendChild(element)
+  element.click()
+  document.body.removeChild(element)
+}
 
 const loadStateLogic = createLogic({
   type: 'LOAD_STATE',
@@ -25,7 +36,19 @@ const toSwaggerJsonLogic = createLogic({
   type: 'TO_SWAGGER_JSON',
   latest: true,
   async process ({ getState, action }, dispatch, done) {
-    console.log('TO_SWAGGER_JSON')
+    const state = getState()
+    const data = JSON.stringify(state, null, 2)
+    if (global.electron) { // electron
+      const filePath = global.electron.dialog.showSaveDialog({
+        filters: [{ name: 'swagger files', extensions: ['json'] }]
+      })
+      if (filePath) {
+        global.fs.writeFileSync(filePath, data)
+        global.electron.shell.showItemInFolder(filePath)
+      }
+    } else { // browser
+      downloadFile(`swagger-${moment().format('YYYYMMDDHHmmss')}.json`, data)
+    }
     done()
   }
 })
